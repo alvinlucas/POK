29cb694152380b695bac97ae5f489d090df5e744
Ambroise Morandeau <ambroise.morandeau@gmail.com>
2022-11-10 12:46:42 +0100

    page projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { WhyPokComponent } from './why-pok/why-pok.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProjetComponent } from './projet/projet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     WhyPokComponent,
-    ContactComponent
+    ContactComponent,
+    ProjetComponent
   ],
   imports: [
     BrowserModule
