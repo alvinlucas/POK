8e0dbce24e0c68f8a8a53fb1a855aff2017fe3df
Ambroise Morandeau <ambroise.morandeau@gmail.com>
2022-11-16 11:42:08 +0100

    création page contactez nous

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { Projet2Component } from './projet2/projet2.component';
 import { Projet3Component } from './projet3/projet3.component';
 import { FooterComponent } from './footer/footer.component';
 import { TeamComponent } from './team/team.component';
+import { ContactezNousComponent } from './contactez-nous/contactez-nous.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { TeamComponent } from './team/team.component';
     Projet2Component,
     Projet3Component,
     FooterComponent,
-    TeamComponent
+    TeamComponent,
+    ContactezNousComponent
   ],
   imports: [
     BrowserModule
