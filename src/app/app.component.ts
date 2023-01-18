c21e66881a6eea80d327231f7a5100911050daeb
alvinlucas <alvin44.lucas@gmail.com>
2023-01-18 15:51:15 +0100

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  @ViewChild('myDiv') myDiv: any
-  ngOnInit(): void {
-    this.myDiv.nativeElement.scrollTop = this.myDiv.nativeElement.scrollWidth 
-  }
-
+export class AppComponent {
   title = 'POK';
 
   public loading: boolean = false;
