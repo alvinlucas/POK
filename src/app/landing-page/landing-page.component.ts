6e38cefefc60269a4fa4ed3a32497c7b66846e18
alvinlucas <alvin44.lucas@gmail.com>
2023-03-22 13:40:16 +0100

    modif

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -11,7 +11,7 @@ export class LandingPageComponent implements OnInit {
 
   constructor(private router: Router) { setTimeout(() => {
     this.router.navigateByUrl('/home');
-  }, 2000); // Redirect after 3 seconds
+  }, 1200); // Redirect after 3 seconds
 }
 
   ngOnInit(): void {
